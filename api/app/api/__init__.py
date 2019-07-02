7cd82409242f155aae1b31128113fa56deff05cd
Anurag Choudhary <anurag.choudhary@geminisolutions.in>
2019-07-02 18:46:17 +0530

    Corrected the name of Helpdesk

--- api/app/api/__init__.py
+++ api/app/api/__init__.py
@@ -2,7 +2,7 @@ from flask_restplus import Api
 from app.api.ticket.apiController import api as ns1
 
 api = Api(version='1.0',
-          title='Helpdesl Rest API',
+          title='Helpdesk Rest API',
           doc='/swagger',
           description='Document for Helpdesk REST API',
           contact='helpdesk-developers@geminisolutions.in',
